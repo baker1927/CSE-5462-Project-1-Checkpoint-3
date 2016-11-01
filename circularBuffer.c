d034fcd8c119ffbff8dc69e193c572c1c52de016
baker1927 <baker.1927@osu.edu>
2016-11-01 19:20:54 -0400

    Sliding window now works for files less than buffer size.

--- circularBuffer.c
+++ circularBuffer.c
@@ -2,10 +2,48 @@
 
 /* Prototypes in header ^^^ */
 
-void AddToBuffer(char *p)
+int AddToBuffer(char *p, struct node *temp)
 {
-    cBuffer[end] = p;
+	
+	int ready = 0;
+	
+	
+	/* Node to get info on current buffer slot of seq */
+	while (ready == 0) {
+		struct node *ptr;
+		ptr = (struct node *)malloc(sizeof(struct node));
+		ptr->next = NULL;
+		ptr = findNode(temp, end);
+			
+		if (ptr != NULL) {
+			
+			if (ptr->ack == 1) {
+				
+				//buffer slot is ready
+				ready = 1;
+			} else {
+				// increment buffer slot
+				end = (end + MSS) % CBUFFERSIZE;
+			}
+		}
+		else {
+			// buffer slot ready
+			ready = 1;
+		}
+	}
+	
+	
+	int temp2 = end;
+	
+	//cBuffer[end] = p;
+	
+    cBuffer[end] = malloc(MSS);
+	
+	bcopy(p, cBuffer[end], MSS);
+
+	//strcpy(cBuffer[end], p);
     end = (end + MSS) % CBUFFERSIZE;
+
 
     if (active <= CBUFFERSIZE)
     {
@@ -13,6 +51,36 @@ void AddToBuffer(char *p)
     } else {
         start = (start + MSS) % CBUFFERSIZE;
     }
+	
+	
+	return temp2;
+}
+
+int AddToBufferForServer(char *p)
+{
+	
+	
+	int temp2 = end;
+	
+	//cBuffer[end] = p;
+	
+    cBuffer[end] = malloc(MSS);
+	
+	bcopy(p, cBuffer[end], MSS);
+
+	//strcpy(cBuffer[end], p);
+    end = (end + MSS) % CBUFFERSIZE;
+
+
+    if (active <= CBUFFERSIZE)
+    {
+       active = active + MSS;
+    } else {
+        start = (start + MSS) % CBUFFERSIZE;
+    }
+	
+	
+	return temp2;
 }
 
 char * GetFromBuffer()
@@ -33,12 +101,14 @@ char * GetFromBuffer()
 char * GetFromBufferByIndex(int index)
 {
 	char *p;
-
-	if (!active) {
+	printf("In Get Buffer\n");
+	fflush(stdout);
+	/*if (!active) {
 		return NULL;
-	}
+	}*/
 	
 	p = cBuffer[index];
+	//printf("Copied data from buffer slot: %d\n", index);
 	/*
 	start = (start + MSS) % CBUFFERSIZE;
 
@@ -56,13 +126,20 @@ int cBufferFull() {
 	}
 }
 
-int cBufferEmpty() {
-	if (start == 0) {
-		return 1;
+int cBufferReady(struct node *temp) {
+	int i = 0;
+	
+	for (i = 0; i < CBUFFERSIZE; i = (i + MSS)) {
+		struct node *ptr;
+		ptr = (struct node *)malloc(sizeof(struct node));
+		ptr->next = NULL;
+		ptr = findNode(temp, i);
+		
+		if (ptr->ack == 0) {
+			return 0;
+		}
 	}
-	else {
-		return 0;
-	}
+	return 1;
 }
 
 int getStart() {
